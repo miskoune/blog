c584c5a4a6e5c9569a1ad28ef3ecc0754a1eac7c
miskoune <miskoune@proton.me>
2025-10-18 02:19:14 +0200

    Update Astro configuration to set trailingSlash to 'never' and ensure proper formatting with a newline at the end of the file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://miskoune.com',
-
+  trailingSlash: 'never',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
